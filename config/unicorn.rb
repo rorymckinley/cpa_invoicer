7d39ef014155681f4367bbfc46a5edd1a27c090b
Rory McKinley <rorymckinley@gmail.com>
2013-12-27 18:12:46 +0200

    Up Unicorn timeout
    * To allow for the longer running uploads

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -30,7 +30,7 @@ listen APP_PATH + "/tmp/sockets/.unicorn.sock", :backlog => 64
 # listen 8080, :tcp_nopush => true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
-timeout 30
+timeout 60
 
 # feel free to point this anywhere accessible on the filesystem
 pid APP_PATH + "/tmp/pids/unicorn.pid"
